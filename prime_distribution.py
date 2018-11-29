e79469da3aca100ec5f3b022cc871d1834969a21
robotslikemars <spoonwood@naver.com>
2018-11-29 08:51:09 -0500

    added detail re:largest prime  in dataset to prime_distribution.

--- prime_distribution.py
+++ prime_distribution.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 def prime_dist():
 
-    print("Q1: See imported list of a million primes in primes1.txt.")
+    print("Q1: See imported list of a million primes in primes1.txt. Contains prime numbers up to 15485863.")
 
     primes = []
     primes = open("primes1.txt").read().split()
@@ -32,9 +32,6 @@ def prime_dist():
     print("Q2: Percentage of first million primes ending in 1 is "+str(num1/10000)+"%, ending in 3 is "+str(num3/10000)+"%, ending in 7 is "+str(num7/10000)+"%, ending in 9 is "+str(num9/10000)+"%.")
 
     a = prettytable.PrettyTable(["Last Digit","Followed by Last Dig = 1", "Followed by Last Dig = 3", "Followed by Last Dig = 7", "Followed by Last Dig = 9"])
-    # b = prettytable.PrettyTable(["Followed by Ending in:", "Ending in 3:"])
-    # c = prettytable.PrettyTable(["Followed by Ending in:", "Ending in 7:"])
-    # d = prettytable.PrettyTable(["Followed by Ending in:", "Ending in 9:"])
 
     #Question 3 and 4
     pos = int(0)
@@ -112,27 +109,6 @@ def prime_dist():
     a.add_row([7,q3c1,q3c3,q3c7,q3c9])
     a.add_row([9,q3d1,q3d3,q3d7,q3d9])
     print("\nQ3: \n",a)
-
-    # a.add_row([1, q3a1])
-    # a.add_row([3, q3a3])
-    # a.add_row([7, q3a7])
-    # a.add_row([9, q3a9])
-    # print("\nQ3a: \n",a)
-    # b.add_row([1, q3b1])
-    # b.add_row([3, q3b3])
-    # b.add_row([7, q3b7])
-    # b.add_row([9, q3b9])
-    # print("\nQ3b: \n",b)
-    # c.add_row([1, q3c1])
-    # c.add_row([3, q3c3])
-    # c.add_row([7, q3c7])
-    # c.add_row([9, q3c9])
-    # print("\nQ3c: \n",c)
-    # d.add_row([1, q3d1])
-    # d.add_row([3, q3d3])
-    # d.add_row([7, q3d7])
-    # d.add_row([9, q3d9])
-    # print("\nQ3d: \n",d)
 
     print("Q4: The number of twin primes is: ", numtwins)
 
