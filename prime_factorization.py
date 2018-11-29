825b12d7cb97291ddad121e6a881477781974bbb
robotslikemars <8419233+robotslikemars@users.noreply.github.com>
2018-11-28 22:33:10 -0500

    Update prime_factorization.py

--- prime_factorization.py
+++ prime_factorization.py
@@ -1,17 +1,17 @@
-  import math
+import math
 
-  def trial_division(input):
+def trial_division(input):
 
-      n = int(input)
-      factorization = []
+    n = int(input)
+    factorization = []
 
       #start w smallest possible prime factor
-      minf = 2
-      while n>1:
-          if n%f ==0:
-              factorization.append(f)
-              n= n/f
-          else:
-              f = f+1
+    minf = 2
+    while n>1:
+        if n%f ==0:
+            factorization.append(f)
+            n= n/f
+        else:
+            f = f+1
 
-      return factorization
+    return factorization
