b51dd9c8852470f683e297cacbe92febfba973f9
cmc2374 <45111385+cmc2374@users.noreply.github.com>
2018-11-29 23:01:30 -0500

    Add files via upload
    Minor tweaks to update new Sieve.py

--- Primality.py
+++ Primality.py
@@ -37,14 +37,39 @@ def sieve(input):
         i = i + 1
 
     #Sieve Algorithm to find primes
-    for y in range(2, int(math.sqrt(n))):
-        for x in range( y + 1, n):
+    ybank = []
+
+
+    for y in range(2, int((math.sqrt(n))) + 1):
+        y_test = 0
+        #Speed buffs
+        for z in ybank:
+            if y % z == 0:
+                y_test = 1
+                break
+        if y_test == 1:
+
+            continue
+
+        ybank.append(y)
+
+
+        for x in range( y + y ,  n , y):
+            if list[x] == 0:
+                continue
+
+            if y > x:
+                print(x)
+                break
             if x % y == 0:
                 list.pop(x)
                 list.insert(x,0)
 
    #Place all primes in a clean list
-    for z in range(0, n):
+
+    list.pop(1)
+    list.insert(1,0)
+    for z in range(0, n ):
         if list[z] != 0:
             final_list.append(list[z])
 
